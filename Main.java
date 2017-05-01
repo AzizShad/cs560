4317de5fca3c78d820d32af56463b1f60cd7ec1a
ayessamedrano <ayessamedrano@yahoo.com>
2017-04-30 17:14:13 -0700

    Algorithm added and path discovered

--- Main.java
+++ Main.java
@@ -18,47 +18,124 @@ public class Main {
         storage = fileParser.parse();
         graph gr = grapher.create(storage);
         
-        //Looks at the values at edges of a hex(start)
-        int start = 1;
+        //------------------------------------
+        int start = 226; //starting point in graph
+        //start already declared on top
+        int end = 8;
+        int v;
+        int w;
+        int weight;
+        int dist;
+        int maxVertices = 233;
+        boolean intree[] = new boolean[maxVertices + 1];
+        int distance[] = new int[maxVertices + 1];
+        int parent[] = new int[maxVertices + 1];
         
-        if(gr.containsEdge(gr.getEdge(start, start+15))){
-        	System.out.println("\t\ttop: " + gr.getEdgeWeight(gr.getEdge(start,start+15 )));
+        for(int i=1; i <= maxVertices; i++){ //Initializing all array contents
+        	intree[i] = false;
+        	distance[i] = Integer.MAX_VALUE; 
+        	parent[i] = -1;
         }
-        if(gr.containsEdge(gr.getEdge(start, start-8))){
-        	System.out.print("top left: " + gr.getEdgeWeight(gr.getEdge(start, start-8)));
+        distance[start] = 0;
+        v = start;
+        
+        while(!intree[v]){
+        	intree[v] = true;
+
+        	//System.out.println("parent: " + parent[v]);
+        	//System.out.println("v: " + v);
+        	
+        	
+        	//Finds value of current hex
+            //if(gr.containsEdge(gr.getEdge(v+15, v))){
+            	//System.out.println("\t\tCURRENT: " + gr.getEdgeWeight(gr.getEdge(v+15, v)));
+            //}
+            //else if(gr.containsEdge(gr.getEdge(start-8, v))){
+            	//System.out.println("\t\tCURRENT: " + gr.getEdgeWeight(gr.getEdge(start-8, v)));
+            //}
+            //else if(gr.containsEdge(gr.getEdge(v-7, v))){
+            	//System.out.println("\t\tCURRENT: " + gr.getEdgeWeight(gr.getEdge(v-7, v)));
+            //}
+            //else if(gr.containsEdge(gr.getEdge(v+7, v))){
+            	//System.out.println("\t\tCURRENT: " + gr.getEdgeWeight(gr.getEdge(v+7, v)));
+            //}
+            //else if(gr.containsEdge(gr.getEdge(v+8, v))){
+            	//System.out.println("\t\tCURRENT: " + gr.getEdgeWeight(gr.getEdge(v+8, v)));
+            //}
+            //else if(gr.containsEdge(gr.getEdge(start-15, v))){
+            	//System.out.println("\t\tCURRENT: " + gr.getEdgeWeight(gr.getEdge(start-15, start)));
+            //}
+        	if(gr.containsEdge(gr.getEdge(v, v-15))){//if has edge on top
+             	//System.out.println("\t\ttop: " + gr.getEdgeWeight(gr.getEdge(v,v-15 )));
+             	w = v - 15;
+             	weight = (int)gr.getEdgeWeight(gr.getEdge(v, v-15));
+             	if(distance[w]> (distance[v] + weight)){
+             		distance[w] = distance[v] + weight;
+             		parent[w] = v;
+             	}
+             }
+             if(gr.containsEdge(gr.getEdge(v, v-8))){//if has edge on top left
+             	//System.out.print("top left: " + gr.getEdgeWeight(gr.getEdge(v, v-8)));
+             	w = v - 8;
+             	weight = (int)gr.getEdgeWeight(gr.getEdge(v, v-8));
+             	if(distance[w]> (distance[v] + weight)){
+             		distance[w] = distance[v] + weight;
+             		parent[w] = v;
+             	}
+             }
+             if(gr.containsEdge(gr.getEdge(v, v-7))){//if has edge on top right
+             	//System.out.println("\t\t\ttop right: " + gr.getEdgeWeight(gr.getEdge(v, v-7)));
+             	w = v - 7;
+             	weight = (int)gr.getEdgeWeight(gr.getEdge(v, v-7));
+             	if(distance[w]> (distance[v] + weight)){
+             		distance[w] = distance[v] + weight;
+             		parent[w] = v;
+             	}
+             }
+             if(gr.containsEdge(gr.getEdge(v, v+7))){
+             	//System.out.print("bottom left: " + gr.getEdgeWeight(gr.getEdge(v,v+7 )));
+             	w = v + 7;
+             	weight = (int)gr.getEdgeWeight(gr.getEdge(v, v+7));
+             	if(distance[w]> (distance[v] + weight)){
+             		distance[w] = distance[v] + weight;
+             		parent[w] = v;
+             	}
+             }
+             if(gr.containsEdge(gr.getEdge(v, v+8))){
+             	//System.out.println("\t\tbottom right: " + gr.getEdgeWeight(gr.getEdge(v, v + 8)));
+             	w = v + 8;
+             	weight = (int)gr.getEdgeWeight(gr.getEdge(v, v+8));
+             	if(distance[w]> (distance[v] + weight)){
+             		distance[w] = distance[v] + weight;
+             		parent[w] = v;
+             	}
+             }
+             if(gr.containsEdge(gr.getEdge(v, v+15))){
+             	//System.out.println("\t\tbottom: " + gr.getEdgeWeight(gr.getEdge( v,v+15)));
+             	w = v + 15;
+             	weight = (int)gr.getEdgeWeight(gr.getEdge(v, v+15));
+             	if(distance[w]> (distance[v] + weight)){
+             		distance[w] = distance[v] + weight;
+             		parent[w] = v;
+             	}
+             }
+             v = 1;
+             dist = Integer.MAX_VALUE;
+             for(int i = 1; i<=maxVertices; i++){
+            	 if(!intree[i] && (dist > distance[i])){
+            		 dist = distance[i];
+            		 v = i;
+            	 }
+             }
         }
-        if(gr.containsEdge(gr.getEdge(start, start-7))){
-        	System.out.println("\t\t\ttop right: " + gr.getEdgeWeight(gr.getEdge(start, start-7)));
-        }
-        //Finds value of current hex
-        if(gr.containsEdge(gr.getEdge(start+15, start))){
-        	System.out.println("\t\tcurrent: " + gr.getEdgeWeight(gr.getEdge(start+15, start)));
-        }
-        else if(gr.containsEdge(gr.getEdge(start-8, start))){
-        	System.out.println("\t\tcurrent: " + gr.getEdgeWeight(gr.getEdge(start-8, start)));
-        }
-        else if(gr.containsEdge(gr.getEdge(start-7, start))){
-        	System.out.println("\t\tcurrent: " + gr.getEdgeWeight(gr.getEdge(start-7, start)));
-        }
-        else if(gr.containsEdge(gr.getEdge(start+7, start))){
-        	System.out.println("\t\tcurrent: " + gr.getEdgeWeight(gr.getEdge(start+7, start)));
-        }
-        else if(gr.containsEdge(gr.getEdge(start+8, start))){
-        	System.out.println("\t\tcurrent: " + gr.getEdgeWeight(gr.getEdge(start+8, start)));
-        }
-        else if(gr.containsEdge(gr.getEdge(start-15, start))){
-        	System.out.println("\t\tcurrent: " + gr.getEdgeWeight(gr.getEdge(start-15, start)));
-        }
-        //end else if
-        if(gr.containsEdge(gr.getEdge(start, start+7))){
-        	System.out.print("bottom left: " + gr.getEdgeWeight(gr.getEdge(start,start+7 )));
-        }
-        if(gr.containsEdge(gr.getEdge(start, start+8))){
-        	System.out.println("\t\tbottom right: " + gr.getEdgeWeight(gr.getEdge(start, start + 8)));
-        }
-        if(gr.containsEdge(gr.getEdge(start, start-15))){
-        	System.out.println("\t\tbottom: " + gr.getEdgeWeight(gr.getEdge( start,start-15)));
-        }
+        //Backtracking from end(8) to start(226) to get path
+        int f = end;
+        do{
+        	System.out.println("value: " + f + ", parent: " + parent[f]);
+        	f = parent[f];
+        }while(f != start);
+        
+        
         
     }
 }
